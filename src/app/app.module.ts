0a06cca242c71a49e6898889f2f117d604bf9069
fredericomacedo <fredvalentim.macedo@gmail.com>
2023-05-27 00:14:53 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VegetablesComponent } from './vegetables/vegetables.component';
+import { VegetablesListComponent } from './vegetables/vegetables-list/vegetables-list.component';
+import { VegetablesDetailComponent } from './vegetables/vegetables-detail/vegetables-detail.component';
+import { VegetableItemComponent } from './vegetables/vegetables-list/vegetable-item/vegetable-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VegetablesComponent,
+    VegetablesListComponent,
+    VegetablesDetailComponent,
+    VegetableItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/vegetables/vegetables-list/vegetable-item/vegetable-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vegetable-item',
+  templateUrl: './vegetable-item.component.html',
+  styleUrls: ['./vegetable-item.component.css']
+})
+export class VegetableItemComponent {
+
+}
